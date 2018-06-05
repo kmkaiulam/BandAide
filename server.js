6a266788eb06929ab620e93d0bdf19424ae351d4
Kenny Lam <kmlam@ucdavis.edu>
2018-06-05 14:24:39 -0700

    add travis file and test

--- server.js
+++ server.js
@@ -1,4 +1,76 @@
+'use strict';
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+app.use(express.json());
 app.use(express.static('public'));
-app.listen(process.env.PORT || 8080);
+
+/*
+const { router: usersRouter } = require('./users');
+const { router: authRouter, localStrategy, jwtStrategy } = require('./auth');
+*/
+
+const { PORT, DATABASE_URL } = require('./config');
+
+
+// Logging
+app.use(morgan('common'));
+
+// CORS
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+  if (req.method === 'OPTIONS') {
+    return res.send(204);
+  }
+  next();
+});
+
+let server;
+
+// this function connects to our database, then starts the server
+function runServer(databaseUrl, port = PORT) {
+  return new Promise((resolve, reject) => {
+    mongoose.connect(databaseUrl, err => {
+      if (err) {
+        return reject(err);
+      }
+      server = app.listen(port, () => {
+        console.log(`Your app is listening on port ${port}`);
+        resolve();
+      })
+        .on('error', err => {
+          mongoose.disconnect();
+          reject(err);
+        });
+    });
+  });
+}
+
+// this function closes the server, and returns a promise. we'll
+// use it in our integration tests later.
+function closeServer() {
+  return mongoose.disconnect().then(() => {
+    return new Promise((resolve, reject) => {
+      console.log('Closing server');
+      server.close(err => {
+        if (err) {
+          return reject(err);
+        }
+        resolve();
+      });
+    });
+  });
+}
+
+// if server.js is called directly (aka, with `node server.js`), this block
+// runs. but we also export the runServer command so other code (for instance, test code) can start the server as needed.
+if (require.main === module) {
+  runServer(DATABASE_URL).catch(err => console.error(err));
+}
+
+module.exports = { runServer, app, closeServer };
---
+++ test/initTest.js
@@ -0,0 +1,24 @@
+
+const chai = require('chai');
+const expect = require('chai').expect;
+const chaiHttp = require('chai-http');
+const mongoose = require('mongoose');
+
+chai.use(chaiHttp);
+
+const {app, runServer, closeServer} = require ('../server')
+const {TEST_DATABASE_URL} = require('../config');
+
+
+describe('GET endpoint', function(){
+
+    it('should return status 200', function(){
+        let res;
+        return chai.request(app)
+            .get('/')
+            .then(_res => {
+            res = _res;
+            expect(res).to.have.status(200);
+            });
+        });
+    }); 
