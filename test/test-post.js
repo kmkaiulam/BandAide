88455fd970225451359b78254daa55661a1e35fb
Kenny Lam <kmlam@ucdavis.edu>
2018-06-08 11:29:43 -0700

    seed test data for testing

--- test/test-post.js
+++ test/test-post.js
@@ -7,24 +7,70 @@ const mongoose = require('mongoose');
 chai.use(chaiHttp);
 
 const {app, runServer, closeServer} = require ('../server')
-const {TEST_DATABASE_URL} = require('../config');
+const {TEST_DATABASE_URL} = require('../config/constants');
 
 
+function generateTestPost(){
+    const testPost = [{
+        type: 'Event Eval',
+        priority: 'High',
+        team: 'All',
+        topic: 'SWS 20180610',
+        content: 'Let\'s discuss any tech issues or improvements that we can make.', 
+        comments: {
+                topic: 'Wireless',
+                comment: 'Had some issues with the channels we used, DPA sounded like he was inside a bathroom',
+                createdBy: 'Kenny',
+                created: 20180611
+        }
+    }];
+    return testPost
+}
+
+function seedTestData(){
+    const seedData =[]
+        seedData.push(generateTestPost);
+    console.info('seed post data');
+    return Post.insert(seedData);
+}
+
+function dropDatabase(){
+    console.warn('Deleting Database');
+    return mongoose.connection.dropDatabase();
+}
 
 
+describe('BandAide Posts', function() {
 
-// --'Posts' Tests --
-describe('GET endpoint', function(){
+    before(function(){
+        return runServer(TEST_DATABASE_URL);
+    });
 
+    beforeEach(function(){
+        return seedTestData();
+    });
 
+   afterEach(function(){
+        return dropDatabase();
+    });
 
-    it('should return status 200', function(){
-        let res;
-        return chai.request(app)
-            .get('/')
-            .then(_res => {
-            res = _res;
-            expect(res).to.have.status(200);
+    after(function(){
+        return closeServer();
+    });
+
+    // --'Posts' Tests --
+    describe('GET endpoint', function(){
+
+        it('should return status 200', function(){
+            let res;
+            return chai.request(app)
+                .get('/')
+                .then(_res => {
+                res = _res;
+                expect(res).to.have.status(200);
+                });
             });
-        });
-    }); 
+    });
+
+    
+});
