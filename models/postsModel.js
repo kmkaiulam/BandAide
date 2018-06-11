ccd7a5064c8dcdaf5b9fdd3257ee36750512f281
Kenny Lam <kmlam@ucdavis.edu>
2018-06-11 12:34:46 -0700

    sorting out testing

--- models/postsModel.js
+++ models/postsModel.js
@@ -3,18 +3,8 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 const Schema = mongoose.Schema;
-const {User} = require('./');
+const {User, Comment} = require('./');
 const ObjectId = Schema.Types.ObjectId
-
-const CommentSchema = new Schema({
-    topic: {type: String},
-    comment: {type: String},
-    createdBy: {type: ObjectId, ref:'User'}, // how to grab schema from other file? do i need to use require('./usersModel')?
-    created: {type: Date, default: Date.now}
-});
-
-const Comment = mongoose.model('Comment', CommentSchema);
-// going to write comment section in postsRouter to make sure the values are strings
 
 const PostSchema = new Schema({
   type: {
@@ -67,14 +57,9 @@ PostSchema.methods.serialize = function(){
   };
 };
 
-
-
-//how do I grab just one value from another schema? in this case I want 
-//createdBy to automatically populate with the signed in user's username
   
-
 
 
 const Post = mongoose.model('Post', PostSchema);
 
-module.exports = {Post, Comment};
+module.exports = {Post};
