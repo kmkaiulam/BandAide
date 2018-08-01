fd6a959190c1ff4550e34fc78fe57496e447c803
Kenny Lam <kmlam@ucdavis.edu>
2018-08-01 09:53:56 -0700

    update travis and config

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 'use strict';
 
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/post';
-exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-post';
+exports.DATABASE_URL = process.env.DATABASE_URL 
+exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL 
 exports.PORT = process.env.PORT;
 exports.JWT_SECRET = process.env.JWT_SECRET;
-exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
+exports.JWT_EXPIRY = process.env.JWT_EXPIRY;
